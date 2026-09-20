8a4e6953290c03a8551c435cfcd5c788c3643b8d
Satyam Kumar <satyamkumar25923@gmail.com>
2026-09-21 04:01:06 +0530

    Add letter guessing and display functionality
    Implement functionality to replace blanks with guessed letters in Hangman game.

---
+++ Hangman/ReplaceBlankWithGuess.py
@@ -0,0 +1,25 @@
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
+# TODO-1: Create a "placeholder" with the same number of blanks as the chosen_word
+
+
+placeHolder = ""
+wordlength = len(chosen_word)
+for pos in range(wordlength):
+    placeHolder += "_"
+print(placeHolder)
+guess = input("Guess a letter: ").lower()
+
+# TODO-2: Create a "display" that puts the guess letter in the right positions and _ in the rest of the string.
+display = ""
+for letter in chosen_word:
+    if letter == guess:
+       display = display + letter
+    else:
+       display += "_"
+
+print(display)
